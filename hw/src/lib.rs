af7d5421e9fec60fe6d887732af8edde2f01b3c5
PostRex <post-rex@pm.me>
2021-11-01 14:19:18 +0100

    no, it doesn't work :c

--- hw/src/lib.rs
+++ hw/src/lib.rs
@@ -1,8 +1,32 @@
+use wasmer::{imports, Function, Instance, Module, Store, Value, Array};
+
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[no_mangle]
-pub extern "C" fn leak_num() -> *mut u32 {
-    Box::leak(Box::new(0xDEADBEEF))
+pub extern "C" fn run_vm() -> u32 {
+    let store = Store::default();
+    let module = Module::from_binary(
+        &store,
+        include_bytes!("hw.wasm"),
+    )
+        .unwrap();
+
+    let import_objects = imports! {
+        "env" => {
+            // i should have probably removed this
+            "ext_num" => Function::new_native(&store, || 0),
+        },
+    };
+    let instance = Instance::new(&module, &import_objects).unwrap();
+    let mem = instance.exports.get_memory("memory").unwrap().clone();
+
+    let num = instance.exports.get_function("leak_num").unwrap().clone();
+
+    let loc = num.call(&[]).unwrap()[0].i32().unwrap() as u32;
+
+    let ptr = unsafe { mem.data_ptr().add(loc as usize) } as *mut u32;
+
+    unsafe { *ptr }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use wasmer::{imports, Function, Instance, Module, Store, Value, Array};
 
-fn ext_num() -> u32 {
-    321
-}
-
 fn main() {
     let store = Store::default();
     let module = Module::from_binary(
@@ -12,20 +8,12 @@ fn main() {
     )
         .unwrap();
 
-    let import_objects = imports! {
-        "env" => {
-            "ext_num" => Function::new_native(&store, ext_num),
-        },
-    };
+    let import_objects = imports! {};
     let instance = Instance::new(&module, &import_objects).unwrap();
-    let mem = instance.exports.get_memory("memory").unwrap().clone();
 
-    let num = instance.exports.get_function("leak_num").unwrap().clone();
+    let num = instance.exports.get_function("run_vm").unwrap().clone();
 
-    let loc = num.call(&[]).unwrap()[0].i32().unwrap() as u32;
+    let deadbeef = num.call(&[]).unwrap()[0].i32().unwrap() as u32;
 
-    println!("{}", loc);
-    let ptr = unsafe { mem.data_ptr().add(loc as usize) } as *mut u32;
-    println!("{:X}", unsafe { *ptr });
-    println!("{}", unsafe { mem.data_unchecked() }.len());
+    println!("{}", deadbeef);
 }

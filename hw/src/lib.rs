e11ca625ddc5a2340cd8efe187f909c213eee5ed
PostRex <post-rex@pm.me>
2021-11-01 14:53:55 +0100

    yes it works, but wasmi doesnt let me access memory :c

--- hw/src/lib.rs
+++ hw/src/lib.rs
@@ -1,32 +1,25 @@
-use wasmer::{imports, Function, Instance, Module, Store, Value, Array};
+use wasmi::{ModuleInstance, ImportsBuilder, NopExternals, RuntimeValue, ModuleImportResolver, MemoryInstance};
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[no_mangle]
 pub extern "C" fn run_vm() -> u32 {
-    let store = Store::default();
-    let module = Module::from_binary(
-        &store,
-        include_bytes!("hw.wasm"),
-    )
-        .unwrap();
+    let module = wasmi::Module::from_buffer(include_bytes!("hw.wasm")).unwrap();
 
-    let import_objects = imports! {
-        "env" => {
-            // i should have probably removed this
-            "ext_num" => Function::new_native(&store, || 0),
-        },
+    let instance =
+        ModuleInstance::new(
+            &module,
+            &ImportsBuilder::default()
+        )
+            .expect("failed to instantiate wasm module")
+            .assert_no_start();
+
+     let pos = match instance.invoke_export("leak_num", &[], &mut NopExternals).unwrap().unwrap() {
+        RuntimeValue::I32(pos) => pos as u32,
+        _ => panic!(),
     };
-    let instance = Instance::new(&module, &import_objects).unwrap();
-    let mem = instance.exports.get_memory("memory").unwrap().clone();
 
-    let num = instance.exports.get_function("leak_num").unwrap().clone();
-
-    let loc = num.call(&[]).unwrap()[0].i32().unwrap() as u32;
-
-    let ptr = unsafe { mem.data_ptr().add(loc as usize) } as *mut u32;
-
-    unsafe { *ptr }
+    pos
 }
 

341e4191e42e59fccd2de7f0284e68c53c61e3d1
PostRex <post-rex@pm.me>
2021-11-02 00:57:55 +0100

    benchmark?

--- hw/src/lib.rs
+++ hw/src/lib.rs
@@ -1,8 +1,25 @@
+use std::time::Instant;
+
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+extern "C" {
+    pub fn count() -> u64;
+}
 
 #[no_mangle]
 pub extern "C" fn leak_num() -> *mut u32 {
     Box::leak(Box::new(0xDEADBEEF))
 }
 
+#[no_mangle]
+pub extern "C" fn busy() -> u64 {
+    let mut num = 0u64;
+    let count = unsafe { count() };
+
+    loop {
+        let read = unsafe { core::ptr::read_volatile(&num as *const u64) };
+        unsafe { core::ptr::write_volatile(&mut num as *mut u64, read + 1); }
+        if read >= count { break read }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use wasmer::{imports, Function, Instance, Module, Store, Value, Array};
+use std::time::Instant;
 
-fn ext_num() -> u32 {
-    321
+fn count() -> u64 {
+    100_000_000
 }
 
 fn main() {
@@ -14,18 +15,39 @@ fn main() {
 
     let import_objects = imports! {
         "env" => {
-            "ext_num" => Function::new_native(&store, ext_num),
+            "count" => Function::new_native(&store, count),
         },
     };
     let instance = Instance::new(&module, &import_objects).unwrap();
-    let mem = instance.exports.get_memory("memory").unwrap().clone();
+    // let mem = instance.exports.get_memory("memory").unwrap().clone();
 
-    let num = instance.exports.get_function("leak_num").unwrap().clone();
+    let num = instance.exports.get_function("busy").unwrap().clone();
 
-    let loc = num.call(&[]).unwrap()[0].i32().unwrap() as u32;
+    println!("start");
 
-    println!("{}", loc);
-    let ptr = unsafe { mem.data_ptr().add(loc as usize) } as *mut u32;
-    println!("{:X}", unsafe { *ptr });
-    println!("{}", unsafe { mem.data_unchecked() }.len());
+    let start = Instant::now();
+    let _ = num.call(&[]).unwrap();
+    let t = Instant::now() - start;
+    println!("  wasm: {:.4}ms", t.as_secs_f64() * 1000.0);
+    let w = t.as_secs_f64();
+
+    let start = Instant::now();
+    let _ = busy();
+    let t = Instant::now() - start;
+    println!("native: {:.4}ms", t.as_secs_f64() * 1000.0);
+
+    println!("native speedup: {:.4}x", w / t.as_secs_f64());
+}
+
+
+#[no_mangle]
+pub extern "C" fn busy() -> u64 {
+    let mut num = 0u64;
+    let count = count();
+
+    loop {
+        let read = unsafe { core::ptr::read_volatile(&num as *const u64) };
+        unsafe { core::ptr::write_volatile(&mut num as *mut u64, read + 1); }
+        if read >= count { break read }
+    }
 }

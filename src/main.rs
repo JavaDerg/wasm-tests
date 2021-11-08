6cc987a48e413d6c3b3078f3987bdb334d62f115
PostRex <post-rex@pm.me>
2021-11-08 18:41:28 +0100

    Input works ^^

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use crate::addons::loader::ModLoaderPlugin;
 use heron::{Acceleration, AxisAngle, CollisionShape, PhysicMaterial, PhysicsPlugin, RigidBody, RotationConstraints, Velocity};
 use bevy_prototype_lyon::prelude::*;
 use fastrand as rng;
+use bevy::math::Vec3A;
 
 struct Ship(f32);
 
@@ -26,7 +27,7 @@ fn main() {
 
 fn setup(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    for _ in 0..100 {
+    for _ in 0..1 {
         commands.spawn()
             .insert(Ship(100.0))
             .insert_bundle(GeometryBuilder::build_as(
@@ -53,7 +54,8 @@ fn setup(mut commands: Commands) {
                 ],
                 border_radius: Some(1.0),
             },
-            Velocity::from(Vec2::new(100.0 - rng::f32() * 200.0, 100.0 - rng::f32() * 200.0)),
+            // Velocity::from(Vec2::new(100.0 - rng::f32() * 200.0, 100.0 - rng::f32() * 200.0)),
+            Velocity::default(),
             Acceleration::default(),
             PhysicMaterial {
                 restitution: 0.2,
@@ -77,17 +79,25 @@ fn wrap_system(mut query: Query<&mut Transform>, win_dim: Res<Windows>) {
     }
 }
 
-fn handle_kb(keys: Res<Input<KeyCode>>, mut query: Query<&mut Velocity, With<Ship>>) {
+fn handle_kb(keys: Res<Input<KeyCode>>, mut query: Query<(&mut Velocity, &Transform), With<Ship>>) {
+
     if keys.pressed(KeyCode::W) | keys.pressed(KeyCode::Up) {
-        query.iter_mut().for_each(|mut vel| vel.linear.y += 2.0);
+        query.iter_mut().for_each(|(mut vel, tf)| vel.linear += Vec3::from(tf.rotation.mul_vec3a(Vec3A::Y)));
     }
     if keys.pressed(KeyCode::S) | keys.pressed(KeyCode::Down) {
-        query.iter_mut().for_each(|mut vel| vel.linear.y -= 2.0);
+        query.iter_mut().for_each(|(mut vel, tf)| vel.linear -= Vec3::from(tf.rotation.mul_vec3a(Vec3A::Y)));
     }
     if keys.pressed(KeyCode::A) | keys.pressed(KeyCode::Left) {
-        query.iter_mut().for_each(|mut vel| vel.angular = AxisAngle::new(Vec3::Z, vel.angular.angle() - 0.1));
+        query.iter_mut().for_each(|(mut vel, _)| apply_rotation(&mut vel, 0.01));
     }
     if keys.pressed(KeyCode::D) | keys.pressed(KeyCode::Right) {
-        query.iter_mut().for_each(|mut vel| vel.angular = AxisAngle::new(Vec3::Z, vel.angular.angle() + 0.1));
+        query.iter_mut().for_each(|(mut vel, _)| apply_rotation(&mut vel, -0.01));
     }
+}
+
+fn apply_rotation(vel: &mut Velocity, angle: f32) {
+    let onorm = vel.angular.axis().z.is_sign_positive().then(|| 1.0).unwrap_or(-1.0);
+    let new_angle = onorm * vel.angular.angle() + angle;
+    let nnorm = new_angle.is_sign_positive().then(|| 1.0).unwrap_or(-1.0);
+    vel.angular = AxisAngle::new(Vec3::Z * nnorm, new_angle.abs());
 }
